ca3f55bbf039c407007c53a773388f75b4be28f8
Ubuntu <ubuntu@ip-172-26-2-145.ap-northeast-1.compute.internal>
2021-03-20 15:21:28 +0800

    add timezone

--- install.sh
+++ install.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
+function timezone()
+{
+    echo "select timezone: 4/9/1/1  Asia/Shanghai"
+    tzselect
+    echo "TZ='Asia/Shanghai'; export TZ" >> /root/.profile
+    echo "TZ='Asia/Shanghai'; export TZ" >> /home/ubuntu/.profile
+        
+}
 
 function docker()
 {
     apt-get update
+    apt-get upgrade -y
     apt-get remove docker docker-engine docker.io containerd runc
     apt-get install -y \
 	apt-transport-https \
         ca-certificates \
         curl \
         gnupg
+    [[ -f /usr/share/keyrings/docker-archive-keyring.gpg ]] && rm -rf /usr/share/keyrings/docker-archive-keyring.gpg
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo \
 	"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
         $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get update    
-    apt-get install docker-ce docker-ce-cli containerd.io
+    apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
     docker run -ti --rm hello-world
 }
 
+timezone
 docker
